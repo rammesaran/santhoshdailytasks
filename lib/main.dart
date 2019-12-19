2f7ac54dda974f1a4398ce0e015d0f1d19171263
ramkumar <rammesaravana@gmail.com>
2019-12-19 23:17:27 +0530

    added two cites

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,9 @@ class _MyAppState extends State<MyApp> {
     'Goat',
     'elephat',
     'mouse',
+    'Maruthi',
+    'Swift',
+    "Dzire"
   ];
   @override
   Widget build(BuildContext context) {
