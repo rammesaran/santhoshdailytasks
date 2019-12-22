ff2a620ea40d3d2b4bc64cefde7513f6821da2ca
ramkumar <rammesaravana@gmail.com>
2019-12-22 15:12:01 +0530

    listview without model

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:santhoshtasks/model/playermodel.dart';
 import 'package:santhoshtasks/pageone.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,43 +10,80 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List list = [
-    'cat',
-    'dog',
-    'bulb',
-    'code',
-    'Goat',
-    'elephat',
-    'mouse',
-    'Maruthi',
-    'Swift',
-    "Dzire"
+  final List<PlayerMode> playermode = List<PlayerMode>();
+  final List<String> players = ["Sachin", "Dravid", "Dhoni", "Raina"];
+  final List<String> images = [
+    "https://c.ndtvimg.com/2019-07/u16tdj8s_sachin-tendulkar-icc-twitter_625x300_19_July_19.jpg?output-quality=70&output-format=webp&downsize=555:*",
+    "https://s.ndtvimg.com/images/entities/120/rahul-dravid-174.jpg",
+    "https://img.etimg.com/thumb/msid-61166425,width-300,imgsize-126503,resizemode-4/why-after-13-years-mahendra-singh-dhoni-is-still-not-out.jpg",
+    "https://www.mykhel.com/img/2018/10/raina-1538638720.jpg"
   ];
+  final List<int> ranks = [1, 2, 3, 4];
+  final List<String> nations = ['india', 'india', 'india', 'india'];
+  final List<int> odis = [10330, 2323, 6565, 9898];
+  final List<int> tests = [989, 4545, 1212, 2121];
+  final List<String> matchsummerys = [
+    'hai i am sachin',
+    'hai i am dravid',
+    'hai i am dhoni',
+    'hai i am raina'
+  ];
+
+  //PlayerMode playerMode = PlayerMode();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'santhoshTask',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Santhosh Task'),
-        ),
-        body: Center(
+        body: SafeArea(
           child: Container(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return ListTile(
-                    title: Text(list[index]),
-                    leading: Icon(Icons.person),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  FirstPage(second: list[index].toString())));
-                    });
-              },
-              itemCount: list.length,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
+            child: Center(
+              child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) => Divider(
+                  thickness: 2.0,
+                ),
+                itemCount: players.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 50.0,
+                    color: Colors.blue,
+                    child: ListTile(
+                        leading: GestureDetector(
+                          child: Container(
+                            width: 100.0,
+                            height: 100.0,
+                            padding: EdgeInsets.symmetric(vertical: 10.0),
+                            alignment: Alignment.center,
+                            child: CircleAvatar(
+                              child: Icon(
+                                Icons.person,
+                              ),
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                        title: Text(players[index]),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Players(
+                                  name: players[index].toString(),
+                                  images: images[index].toString(),
+                                  ranks: ranks[index].toString(),
+                                  nation: nations[index].toString(),
+                                  odi: odis[index].toString(),
+                                  test: tests[index].toString(),
+                                  summer: matchsummerys[index].toString(),
+                                ),
+                              ));
+                        }),
+                  );
+                },
+                padding: EdgeInsets.all(10.0),
+              ),
             ),
           ),
         ),
--- lib/pageone.dart
+++ lib/pageone.dart
@@ -1,22 +1,95 @@
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatefulWidget {
-  final String second;
-  FirstPage({this.second});
+class Players extends StatefulWidget {
+  final String name;
+  final String images;
+  final String ranks;
+  final String nation;
+  final String odi;
+  final String test;
+  final String summer;
+  Players(
+      {this.name,
+      this.images,
+      this.ranks,
+      this.nation,
+      this.odi,
+      this.summer,
+      this.test});
   @override
-  _FirstPageState createState() => _FirstPageState(second: second);
+  _PlayersState createState() => _PlayersState(
+      playersname: name,
+      playersimage: images,
+      playersrank: ranks,
+      playernation: nation,
+      playerodi: odi,
+      playersummer: summer,
+      playertest: test);
 }
 
-class _FirstPageState extends State<FirstPage> {
-  String second;
+class _PlayersState extends State<Players> {
+  String playersname;
+  String playersimage;
+  String playersrank;
+  String playernation;
+  String playerodi;
+  String playertest;
+  String playersummer;
 
-  _FirstPageState({this.second});
+  _PlayersState(
+      {this.playersimage,
+      this.playersname,
+      this.playersrank,
+      this.playernation,
+      this.playerodi,
+      this.playersummer,
+      this.playertest});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(this.second.toString()),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(playersname),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "OverView",
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image(
+              image: NetworkImage(playersimage),
+            ),
+            Text(
+              playersname,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              playersrank,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              playernation,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              playerodi,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              playertest,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              playersummer,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
